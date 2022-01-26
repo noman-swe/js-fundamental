5ef7664cb8f6ac6559d7c1e4159f9558799368d8
noman-swe <noman.swe130@gmail.com>
2022-01-27 01:24:14 +0600

    array

---
+++ array.js
@@ -0,0 +1,12 @@
+var friendsAge = [11, 13, 17];
+
+var muriChanaChurFee = [32, 54, 75, 4];
+
+// string array
+var nayikas = ['mahi', 'opu', 'jorina'];
+
+var oddNumbers = [1, 3, 5, 7, 9];
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(nayikas);
