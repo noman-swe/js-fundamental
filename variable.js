9a4c8ac4d2bedbd499624a314cbf5bb668204e0c
noman-swe <noman.swe130@gmail.com>
2022-01-27 00:59:58 +0600

    recap

---
+++ variable.js
@@ -0,0 +1,35 @@
+// integer
+var watchPrice = 502;
+watchPrice += 10;
+//console.log(watchPrice);
+
+// String
+var fName = "Noman";
+var lName = "Shibly";
+var fullName = fName + lName;
+//console.log(fullName);
+
+//boolean
+var isExpensive = true;
+var isGood = false;
+
+// float
+var colgatePrice = 40.50;
+
+//incre decree
+var dairyPrice = 10;
+var fishPrice = 500;
+dairyPrice ++;
+//console.log(dairyPrice);
+fishPrice--;
+//console.log(fishPrice);
+
+//numeric string to interger or float
+var phoneNo = "01688126772"
+ phoneNo = parseInt(phoneNo);
+console.log(phoneNo);
+
+var floatString = '25.545';
+var floatNumber = parseFloat(floatString);
+console.log(floatNumber);
+
