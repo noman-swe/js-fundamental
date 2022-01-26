0dc77cdca8876cb4160de9494e0ed2685731e39a
noman-swe <noman.swe130@gmail.com>
2022-01-27 02:15:40 +0600

    my commit of array index

---
+++ array-index.js
@@ -0,0 +1,19 @@
+var books = ['hablu', 'dablu', 'choddoGushti', 'battery', 'peracitamol'];
+
+var batteryIndex = books.indexOf('battery');
+//console.log(batteryIndex);
+
+var numbers = [44, 45, 65, 47, 68];
+var indexOfNumbers = numbers.indexOf(165);
+//console.log(indexOfNumbers);
+
+
+//index values getting
+var secondIndex = books[4];
+//console.log(secondIndex);
+
+
+//array number index value change
+numbers[1] = 100;
+numbers[4] = 200;
+console.log(numbers);
