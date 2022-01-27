4be82e51d10ea5a5e7480167fad5e0d6bedc816b
noman-swe <noman.swe130@gmail.com>
2022-01-28 01:13:33 +0600

    push(), pop() & unshift(), shift()

---
+++ array-push-pop.js
@@ -0,0 +1,18 @@
+// Array push
+var lastBench = ['alam', 'kalam', 'jalam', 'salam'];
+//lastBench.push('Shibly'); //add values in the last position
+//lastBench.unshift('Noman'); //add values in the first position
+
+console.log(lastBench);
+
+//numeric array push
+var friendsAge = [11, 13, 17, 15];
+//friendsAge.push(10);
+console.log(friendsAge);
+
+//remove or pop an element from a array
+//var lastItem = friendsAge.pop();
+//friendsAge.pop()
+friendsAge.shift();
+console.log(friendsAge);
+//console.log(lastItem);
