3d3635b4f65a76b6231f16d1ecaa63cd067a8a7b
noman-swe <noman.swe130@gmail.com>
2022-01-28 01:35:10 +0600

    Comparison Conditions (<,>,==,!=,&&, ||)

---
+++ comparison.js
@@ -0,0 +1,19 @@
+//Comparison operators
+
+//console.log(5<6); //5 is smallar than 6
+//console.log(5>6); //5 is greater than 6
+
+//console.log(5==6); //exact equal
+//console.log(6==6); //equal
+
+//console.log(5!=6); // not equals to
+
+var myLove = 90;
+var yourLove = 100;
+// console.log(myLove > yourLove);
+// console.log(myLove == yourLove);
+// console.log(myLove < yourLove);
+
+//various conditions
+console.log(studyGood && goodPerson); // (and) -->two conditions must have to be true 
+console.log(isBanker || goodResult); // (or) --> one condition have to be true
