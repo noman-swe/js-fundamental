d5864a8077034e1e02e2ed753aebacfdcbe09bc5
noman-swe <noman.swe130@gmail.com>
2022-01-30 13:06:24 +0600

    if else Commit

---
+++ conditions.js
@@ -0,0 +1,62 @@
+var num = 20;
+var roll = 300;
+var id = 161;
+
+// if(num > roll){
+//     console.log('Present Sir');
+// }
+// else if (roll > num && roll < 50){
+//     console.log('fake Student');
+// }
+// else{
+//     console.log('I teacher');
+// }
+
+
+//another example
+
+//  var eggPrice = 5;
+// while ( eggPrice < 15){
+
+//     if (eggPrice < 8) {
+//         console.log('dim dia vaat khamu');
+//     }
+//     else if (eggPrice <= 9 && eggPrice <= 12) {
+//         console.log('taka asy kina check kore dekhte hobe');
+//     }
+//     else {
+//         console.log('alu vortai valo');
+//     }
+//     eggPrice++;
+
+// }
+
+// var eggPrice = 150;
+
+// var myName = 'Shibly';
+ 
+// if (myName === "Shibly"){
+//     console.log("Shibly! U are allowed");
+// }
+// else if(myName = 'Noman'){
+//     console.log("hey Noman");
+// }
+// else{
+//     console.log('user denied');
+// }
+
+
+var green = true;
+var red = false;
+var yellow = "hold on"
+var signal = green;
+
+if (signal === green){
+    console.log("let's cross the road");
+}
+else if (signal === red){
+    console.log("Don't cross now!");
+}
+else{
+    console.log('hold on');
+}
